8ef3df1c45aed68ee099247db723ca33c8ddd5bb
sergiozuckermann <a01024831@itesm.mx>
2023-10-30 20:15:01 -0600

    files can be read

--- Evidencia 1/lector.cpp
+++ Evidencia 1/lector.cpp
@@ -4,60 +4,48 @@
 
 using namespace std;
 
-void readTxt()
+std::string readTxt(std::string mensaje)
 {
-    string mensaje;
-    std::cout << "Ingrese el mensaje o el nombre del archivo de texto: ";
-    cin >> mensaje;
-
-    for (char & c : mensaje)
-    {
-        c = tolower(c);
-    }
-
-    if (mensaje.find(".txt") != string::npos)
-    {
-        ifstream file(mensaje);
-        if (file)
-        {
-            string contenido((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-            std::cout << "File content:" << endl;
-            std::cout << contenido << endl;
-        }
-        else
-        {
-            std::cout << "Archivo no encontrado." << endl;
-        }
-    }
-    else
-    {
-        std::cout << "No es un archivo de texto (.txt), ingresó un mensaje directamente." << endl;
-    }
+    std::string texto;
+    ifstream file(mensaje);
+    texto = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    return texto;
 }
 
 int main()
 {
-    readTxt();
-    std::cout << "Ingrese una opcion: " << endl;
-    std::cout << "1. Opción 1" << endl;
-    std::cout << "2. Opción 2" << endl;
-    std::cout << "3. Salir" << endl;
 
-    int opcion;
-    cin >> opcion;
+    std::string text1=readTxt("transmission1.txt");
+    std::string text2=readTxt("transmission2.txt");
+    std::string virus1=readTxt("mcode1.txt");
+    std::string virus2=readTxt("mcode2.txt");
+    std::string virus3=readTxt("mcode3.txt");
+    std::cout << "codigo1" << text1 << endl;
+    std::cout <<"codigo2" << text2 << endl;
+    std::cout << "virus1" << virus1 << endl;
+    std::cout <<"virus2" << virus2 << endl;
+    std::cout <<"virus3" << virus3 << endl;
 
-    if (opcion == 1) {
-        //Insertar la función que pide
-    }
-    else if (opcion == 2) {
-        //Insertar la función que pide
-    }
-    else if (opcion == 3) {
-        std::cout << "Adios" << endl;
-    }
-    else {
-        std::cout << "Invalido" << endl;
-        main();
-    }
-    return 0;
+    // std::cout << "Ingrese una opcion: " << endl;
+    // std::cout << "1. Opción 1" << endl;
+    // std::cout << "2. Opción 2" << endl;
+    // std::cout << "3. Salir" << endl;
+
+    // int opcion;
+    // cin >> opcion;
+
+    // if (opcion == 1) {
+    //     //Insertar la función que pide
+    // }
+    // else if (opcion == 2) {
+    //     //Insertar la función que pide
+    // }
+    // else if (opcion == 3) {
+    //     std::cout << "Adios" << endl;
+    // }
+    // else {
+    //     std::cout << "Invalido" << endl;
+    //     main();
+    // }
+     return 0;
 }
